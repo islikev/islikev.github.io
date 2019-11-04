9473a72ffeb2e021486465b714f1ffefeeb0de09
islikev <islikeportfolio@gmail.com>
2019-11-04 23:59:02 +0300

    d

---
+++ index.js
@@ -0,0 +1,9 @@
+function discord() {
+  navigator.clipboard.writeText("sasha_shpak#6532");
+}
+function vk() {
+  navigator.clipboard.writeText("vk.com/sadslike");
+}
+function instagram() {
+  navigator.clipboard.writeText("instagram.com/slikesad");
+}
