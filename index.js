ff4b4b41f9b526dd0f21abb6d9fda4dd8c1790df
zeniumtsw <zeniumtsw@gmail.com>
2019-11-21 23:16:18 +0300

    j

--- index.js
+++
@@ -1,9 +0,0 @@
-function discord() {
-  navigator.clipboard.writeText("sasha_shpak#6532");
-}
-function vk() {
-  navigator.clipboard.writeText("vk.com/sadslike");
-}
-function mail() {
-  navigator.clipboard.writeText("islikeportfolio@gmail.com");
-}
